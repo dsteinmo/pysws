f13e0ceaf00b788f7ea4fd9804e952616bfc65a4
Derek Steinmoeller <dsteinmo@gmail.com>
2017-01-11 21:32:30 -0500

    Introduce test for cheb_dif function.

---
+++ src/test/cheb.py
@@ -0,0 +1,39 @@
+import unittest
+import sys
+from os import path
+from numpy.linalg import norm
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+import spatial_discretizations.cheb.cheb as cheb
+import numpy as np
+
+EPS = 2.e-8
+
+class ChebTests(unittest.TestCase):
+    def test_cheb_dif(self):
+        x, DM = cheb.cheb_dif(4, 2)
+
+        x_expected = np.array([1., 0.5, -0.5, -1.])
+
+        self.assertLess(norm(x-x_expected, ord=2), EPS, "cheb grid")
+
+        D = DM[:, :, 0]
+        D_expected = np.array([[ 3.16666667, -4.,          1.33333333, -0.5       ],
+                               [ 1.,         -0.33333333, -1.,          0.33333333],
+                               [-0.33333333,  1.,          0.33333333, -1.        ],
+                               [ 0.5,        -1.33333333,  4.,         -3.16666667]])
+
+        self.assertLess(norm(D-D_expected, ord=2), EPS, "cheb first derivative matrix")
+
+        D2 = DM[:, :, 1]
+
+        D2_expected = np.array([[5.33333333, -9.33333333,  6.66666667, -2.66666667],
+                                [3.33333333, -5.33333333,  2.66666667, -0.66666667],
+                                [-0.66666667, 2.66666667, -5.33333333, 3.33333333],
+                                [-2.66666667,  6.66666667, -9.33333333,  5.33333333]])
+
+        print norm(D2-D2_expected, ord=2)
+        self.assertLess(norm(D2-D2_expected, ord=2), EPS, "cheb second derivative matrix")
+
+if __name__ == '__main__':
+    unittest.main()
