cd4b121778fd9cd2b6be2efadc64d28216339ebb
Derek Steinmoeller <dsteinmo@gmail.com>
2017-01-22 19:29:20 -0500

    Fix errors/ indentation in Fourier-Cheby class.

--- src/spatial_discretizations/FourierChebyshevSpatialDiscretization.py
+++ src/spatial_discretizations/FourierChebyshevSpatialDiscretization.py
@@ -35,44 +35,43 @@ class FourierChebyshevSpatialDiscretization:
         self.lmax = max(l_1d)
         self.k, self.l = np.meshgrid(k_1d, l_1d)
 
-    if __name__ == '__main__':
-        def __build_filter__(self):
-            cutoff = 0.65  # 0.65 typically,  CUTOFF OF 0 CORRESPONDS TO HYPERVISCOSITY
-            epsf = 1e-15  # FILTER STRENGTH AT HIGH WAVENUMBERS
+    def __build_filter__(self):
+        cutoff = 0.65  # 0.65 typically,  CUTOFF OF 0 CORRESPONDS TO HYPERVISCOSITY
+        epsf = 1e-15  # FILTER STRENGTH AT HIGH WAVENUMBERS
 
-            kcrit = self.kmax * cutoff
-            lcrit = self.lmax * cutoff
-            filter_order = 4
+        kcrit = self.kmax * cutoff
+        lcrit = self.lmax * cutoff
+        filter_order = 4
 
-            self.filter_x = np.ones([self.num_points_y, self.num_points_x])
-            self.filter_y = np.ones([self.num_points_y, self.num_points_x])
+        self.filter_x = np.ones([self.num_points_y, self.num_points_x])
+        self.filter_y = np.ones([self.num_points_y, self.num_points_x])
 
-            # Filter in x.
-            mask = np.abs(self.k) < kcrit
-            self.filter_x *= (
-                mask + (1 - mask) * np.exp(np.log(epsf) * (np.power((self.k - kcrit) / (self.kmax - kcrit), filter_order))))
+        # Filter in x.
+        mask = np.abs(self.k) < kcrit
+        self.filter_x *= (
+            mask + (1 - mask) * np.exp(np.log(epsf) * (np.power((self.k - kcrit) / (self.kmax - kcrit), filter_order))))
 
-            # Filter in y.
-            mask = np.abs(self.l) < lcrit
-            self.filter_y *= (
-                mask + (1 - mask) * np.exp(np.log(epsf) * (np.power((self.l - lcrit) / (self.lmax - lcrit), filter_order))))
+        # Filter in y.
+        mask = np.abs(self.l) < lcrit
+        self.filter_y *= (
+            mask + (1 - mask) * np.exp(np.log(epsf) * (np.power((self.l - lcrit) / (self.lmax - lcrit), filter_order))))
 
-            # Remove the Nyquist frequency entirely
-            self.filter_x[:, self.num_points_x - 1] = 0
-            self.filter_y[np.floor(self.num_points_y / 2 + 1), :] = 0
+        # Remove the Nyquist frequency entirely
+        self.filter_x[:, self.num_points_x - 1] = 0
+        self.filter_y[int(np.floor(self.num_points_y / 2 + 1)), :] = 0
 
-            def differentiate_x(self, field):
-                return cheb.cheb_derivative(field, axis=1)
+    def differentiate_x(self, field):
+        return cheb.cheb_derivative(field, axis=1)
 
-            def differentiate_y(self, field):
-                return np.real(ifft((1.j * self.l) * fft(field, axis=0), axis=0))
+    def differentiate_y(self, field):
+        return np.real(ifft((1.j * self.l) * fft(field, axis=0), axis=0))
 
-            def filter_field(self, field):
-                # Filter in x with cosine transform:
-                field = (1 / (2 * (self.num_points_x - 1))) * cheb.cosine_transform(
-                                self.filter_x * cheb.cosine_transform(field, 1), 1);
+    def filter_field(self, field):
+        # Filter in x with cosine transform:
+        field = (1 / (2 * (self.num_points_x - 1))) * cheb.cosine_transform(
+                        self.filter_x * cheb.cosine_transform(field, 1), 1);
 
-                # Filter in y with Fourier transform:
-                field = ifft(self.filter_y * fft(field, axis=0), axis=0)
+        # Filter in y with Fourier transform:
+        field = ifft(self.filter_y * fft(field, axis=0), axis=0)
 
-                return field
+        return field
