Add filter test for Fourier-Cheby spatial discretization. Fix bug where cosine transform scaling factor was being computed as zero.

src/spatial_discretizations/FourierChebyshevSpatialDiscretization.py
@@ -70,10 +70,10 @@ class FourierChebyshevSpatialDiscretization:
 
     def filter_field(self, field):
         # Filter in x with cosine transform:
-        field = (1 / (2 * (self.num_points_x - 1))) * cheb.cosine_transform(
-                        self.filter_x * cheb.cosine_transform(field, 1), 1);
+        field = (1. / (2. * (self.num_points_x - 1.))) * cheb.cosine_transform(
+                         self.filter_x * cheb.cosine_transform(field, axis=1), axis=1)
 
         # Filter in y with Fourier transform:
-        field = ifft(self.filter_y * fft(field, axis=0), axis=0)
+        field = np.real(ifft(self.filter_y * fft(field, axis=0), axis=0))
 
         return field

src/spatial_discretizations/cheb/cheb.py
@@ -77,9 +77,9 @@ def cheb_derivative(f, axis=0):
 
     dd[N-2, :] = 2*(N-1)*cc[N-1, :]
     for k in range(N-3, -1, -1):
-        dd[k, :] = dd[k+2, :] + 2*(k+1)*cc[k+1, :]
+        dd[k, :] = dd[k+2, :] + 2.*(k+1.)*cc[k+1, :]
 
-    dd = cosine_transform(np.real(dd))/(2*(N-1))
+    dd = cosine_transform(np.real(dd))/(2.*(N-1.))
 
     if axis == 1:
         return dd.T